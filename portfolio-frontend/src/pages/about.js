087744be47820f93fce6b7e4ba153ec870e25b4d
mikesundq <55479605+mikesundq@users.noreply.github.com>
2020-10-08 00:38:49 +0200

    added about must fix

--- portfolio-frontend/src/pages/about.js
+++ portfolio-frontend/src/pages/about.js
@@ -3,9 +3,68 @@ import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
+import ReactMarkdown from "react-markdown"
+import downloadFile from "../../static/MittCV.pdf"
+import { Link } from "gatsby"
 // ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
+
+const About = ({ data }) => {
+    //console.log(data)
+
+    const { info, stack, title, image } = data.allStrapiAbout.nodes[0]
+
+    console.log(image.childImageSharp.fluid)
+
+    // return (
+    //   <h1>about page</h1>
+    // )
+    return (
+        <Layout>
+            <div className="section-center about-center">
+                <image
+                    fluid={image.childImageSharp.fluid}
+                    className="about-img"
+                />
+                <ReactMarkdown source={info} />
+                <div className="about-stack">
+                    {stack.map(item => {
+                        return <span key={item.id}>{item.title}</span>
+                    })}
+                </div>
+                <div className="btn cv">
+                    <a href={downloadFile} download>
+                        Ladda ner fullständigt CV
+                    </a>
+                    {/* {``} */}
+                </div>
+                <Link to="/contact" className="btn">
+                    kontakta mig
+                </Link>
+            </div>
+        </Layout>
+    )
 }
+
+export const query = graphql`
+    {
+        allStrapiAbout {
+            nodes {
+                title
+                info
+                stack {
+                    title
+                    id
+                }
+                image {
+                    childImageSharp {
+                        fluid {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
 
 export default About
