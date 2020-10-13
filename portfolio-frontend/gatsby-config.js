9baa35420d8b8a9f0f098efb899c13f1eda2cd29
mikesundq <55479605+mikesundq@users.noreply.github.com>
2020-10-13 10:41:35 +0200

    Update gatsby-config.js

--- portfolio-frontend/gatsby-config.js
+++ portfolio-frontend/gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     author: "@mike",
     twitterUsername: "@MikeSundqvist",
     image: "/twitter-img.png",
-    siteUrl: "https://mikael-student-portfolio.netlify.app",
+    siteUrl: "https://mike-student-portfolio.netlify.app",
   },
   plugins: [
     `gatsby-transformer-sharp`,
