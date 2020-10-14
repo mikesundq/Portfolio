cadb3e63f58e0e9138bdf354cc7bbd43b0ae5700
mikesundq <55479605+mikesundq@users.noreply.github.com>
2020-10-14 21:08:03 +0200

    removed main css from ssr..

--- portfolio-frontend/gatsby-ssr.js
+++ portfolio-frontend/gatsby-ssr.js
@@ -1,4 +1,4 @@
-import "./src/css/main.css"
+//import "./src/css/main.css"
 import React from "react"
 import { createGlobalStyle, ThemeProvider } from "styled-components"
 import Theme from "./src/themes/theme"
